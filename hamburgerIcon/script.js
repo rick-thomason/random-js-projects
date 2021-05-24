2b12e11c9d4ee9720d2a846b05a9633e6be5f9a2
Rick <67919881+BuRNoceros@users.noreply.github.com>
2021-05-24 19:26:07 -0400

    finish hamburger icon

---
+++ hamburgerIcon/script.js
@@ -0,0 +1,7 @@
+const button = document.getElementById('btn')
+const nav = document.getElementById('nav')
+
+button.addEventListener('click', () => {
+  nav.classList.toggle('active')
+  btn.classList.toggle('active')
+})
