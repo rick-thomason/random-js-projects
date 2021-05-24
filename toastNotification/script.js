a3f86ec66b3f0440c659388d728c4bb09d3a443d
Rick <67919881+BuRNoceros@users.noreply.github.com>
2021-05-24 19:39:03 -0400

    add notification to bottom right of screen

---
+++ toastNotification/script.js
@@ -0,0 +1,19 @@
+const btn = document.getElementById('btn')
+const container = document.getElementById('container')
+
+btn.addEventListener('click', () => {
+  createNotification()
+})
+
+function createNotification() {
+  const notification = document.createElement('div')
+  notification.classList.add('toast')
+
+  notification.innerText = 'THIS CHALLENGE IS CRAZY!!!'
+
+  container.appendChild(notification)
+
+  setTimeout(() => {
+    notification.remove()
+  }, 3000)
+}
