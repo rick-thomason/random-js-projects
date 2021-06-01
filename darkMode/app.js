38b729b380263a42e5b41d1f313641ba52eaf087
Rick <67919881+BuRNoceros@users.noreply.github.com>
2021-06-01 19:50:20 -0400

    add some extra styling to darkMode

--- darkMode/app.js
+++ darkMode/app.js
@@ -1,11 +1,11 @@
-const btn = document.querySelector('button')
+const btn = document.getElementById('btn')
 const container = document.querySelector('.container')
 
 function darkMode() {
   btn.addEventListener('click', () => {
     document.body.classList.toggle('dark')
-    btn.classList.toggle('white-background')
     container.classList.toggle('dark')
+    btn.classList.toggle('dark')
   })
 }
 
